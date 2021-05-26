cc619aea5a8a22193db365ca231c3cdca732c2d0
Sayantan-Basak <sayantan.basak.99@gmail.com>
2021-05-26 14:58:52 +0530

    done

--- EPSProj/src/main/java/com/mph/controller/EmployeeRestController.java
+++ EPSProj/src/main/java/com/mph/controller/EmployeeRestController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mph.dao.EmployeeDao;
@@ -26,7 +27,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 * @version 1.0
 */
 @RestController
-@CrossOrigin(origins = "http://localhost:4200",allowCredentials = "false",methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE}, allowedHeaders = "*")
+@RequestMapping("/employee")
+@CrossOrigin(origins = /*"http://localhost:4200"*/"*",allowCredentials = "false",methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE}, allowedHeaders = "*")
 public class EmployeeRestController {
 
 	@Autowired
